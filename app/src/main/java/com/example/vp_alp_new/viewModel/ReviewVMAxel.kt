0e595876ae2018c4dd5ea0a1b7c38a4ea74b7419
BryanSamuelIan <bryansamuelangka@gmail.com>
2024-01-03 16:36:41 +0700

    Axel fix your code plsss

---
+++ app/src/main/java/com/example/vp_alp_new/viewModel/ReviewVMAxel.kt
@@ -0,0 +1,7 @@
+package com.example.vp_alp_new.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class ReviewVMAxel : ViewModel() {
+    // Add necessary properties and functions for managing review data
+}
