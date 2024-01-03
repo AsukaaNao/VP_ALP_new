8cae1fbe0e12aab8bd077acdba0d3d0c1cf75a3b
BryanSamuelIan <bryansamuelangka@gmail.com>
2024-01-03 15:20:49 +0700

    tes

---
+++ app/src/main/java/com/example/vp_alp_new/ui/view/landing.kt
@@ -0,0 +1,49 @@
+package com.example.vp_alp_new.ui.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.res.painterResource
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.vp_alp.R
+
+@Composable
+fun landing() {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = null,
+            modifier = Modifier.size(300.dp)
+        )
+
+
+
+    }
+
+
+
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun landingPreview(){
+    landing()
+
+}
